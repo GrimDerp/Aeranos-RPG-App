1982cf69d18ba96981bafd7bf5efc4b7bbbc8c14
BC <admin@superzen.org>
2021-04-27 13:10:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
-/* rores feelin fancy btw */
+/* totes feelin fancy btw */
 // prints "hi" in the browser's dev tools console
 console.log("hi");
